f9dd04329f1f5b19493beebc0bd2b7df342fd304
JayusJay <atayure@gmail.com>
2024-07-22 20:00:53 +0000

    chore: refactor Gaussian.sol for improved readability and performance

--- src/Gaussian.sol
+++ src/Gaussian.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-
 contract Gaussian {
     int256 internal constant ONE = 1e18; // 18 decimal fixed-point representation of 1
     int256 internal constant HALF = 5e17; // 18 decimal fixed-point representation of 0.5
@@ -22,102 +21,57 @@ contract Gaussian {
     int256 internal constant expConst = -1265512230000000000;
     int256 internal constant TWELFTH = 83333333333333333;
 
-
-     // Function to calculate the Gaussian CDF for fixed-point x, mu, sigma
-     /**
-     CDF formula: 0.5 * (1 + erf((x - mu) / (sigma * sqrt(2))))     
-
-     x = mean
-     mu = variance
-     sigma = SD
-      */
+    // Function to calculate the Gaussian CDF for fixed-point x, mu, sigma
+    /**
+     * CDF formula: 0.5 * (1 + erf((x - mu) / (sigma * sqrt(2))))
+     *
+     *  x = mean
+     *  mu = variance
+     *  sigma = SD
+     */
     function gaussianCDF(int256 x, int256 mu, int256 sigma) public pure returns (int256, int256) {
         int256 z = ((x - mu) * ONE) / (sigma * SQRT_TWO);
-        int256 erfValue = erf(z);
+        //int256 erfValue = erf(z);
         int256 erfcValue = erfc(z);
-        return (((erfValue + ONE) / TWO), erfcValue / 2 );
-    }
-
-    // Function to calculate e^(-x^2/2) for fixed-point x
-     function expNegHalf(int256 x) internal pure returns (int256) {
-        int256 xx = (x * x) / ONE;  // x^2 / 1e18
-        int256 x4 = (xx * xx) / ONE; // x^4 / 1e36
-
-        ///@notice using Padé 2 / 2 over polynomial series to avoid looping
-        ///chosing 2 / 2 over 1 / 1 for better accuracy
-
-        // Compute numerator and denominator for the [2/2] Padé approximant
-        int256 numerator = ONE - (xx / 2) + (x4 / 24);
-        int256 denominator = ONE + (xx / 2) + (x4 / 24);
-
-        // Return the result of the Padé approximant
-        return numerator / denominator;
+        return (1, erfcValue / TWO);
     }
 
     /**
-     var r = t * Math.exp(-z * z - 1.26551223 + t * (1.00002368 +
-            t * (0.37409196 + t * (0.09678418 + t * (-0.18628806 +
-            t * (0.27886807 + t * (-1.13520398 + t * (1.48851587 +
-            t * (-0.82215223 + t * 0.17087277))))))))) */
+     * var r = t * Math.exp(-z * z - 1.26551223 + t * (1.00002368 +
+     *         t * (0.37409196 + t * (0.09678418 + t * (-0.18628806 +
+     *         t * (0.27886807 + t * (-1.13520398 + t * (1.48851587 +
+     *         t * (-0.82215223 + t * 0.17087277)))))))))
+     */
 
-        // Complementary error function approximation
+    // Complementary error function approximation
     function erfc(int256 x) public pure returns (int256) {
         int256 z = x >= 0 ? x : -x;
-        int256 t = (ONE * ONE) / (ONE + ((z * ONE) / 2 ));
-       
-        int256 expTerm = -z * z / ONE - 1265512230000000000;
+        int256 t = (ONE * ONE) / (ONE + ((z * ONE) / 2));
+
+        int256 expTerm = -((z * z) / ONE) - 1265512230000000000;
 
         int256 polynomial = c1;
-        polynomial = addSafe(polynomial, mulDiv(t, c2));
-        polynomial = addSafe(polynomial, mulDiv(t, mulDiv(t, c3)));
-        polynomial = addSafe(polynomial, mulDiv(t, mulDiv(t, mulDiv(t, c4))));
-        polynomial = addSafe(polynomial, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, c5)))));
-        polynomial = addSafe(polynomial, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, c6))))));
-        polynomial = addSafe(polynomial, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, c7)))))));
-        polynomial = addSafe(polynomial, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, c8))))))));
-        polynomial = addSafe(polynomial, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, c9)))))))));
-        int256 r = t * exp(expTerm + polynomial / ONE);
+        polynomial += mulDiv(t, c2);
+        polynomial += mulDiv(t, mulDiv(t, c3));
+        polynomial += mulDiv(t, mulDiv(t, mulDiv(t, c4)));
+        polynomial += mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, c5))));
+        polynomial += mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, c6)))));
+        polynomial += mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, c7))))));
+        polynomial += mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, c8)))))));
+        polynomial += mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, mulDiv(t, c9))))))));
+        int256 r = t * exp((expTerm + polynomial) / ONE);
 
-         return x >= 0 ? r : TWO - r;
+        return x >= 0 ? r : TWO - r;
     }
 
     // Function to calculate the error function approximation for fixed-point x
-    function erf(int256 x) internal pure returns (int256) {
-        int256 sign;
-        if(x < 0){
-            sign = -ONE;
-            x = -x;
-        }else{
-            sign = ONE;
-        }
 
-        // Polynomial approximation constants (maximum error: 1.5×10−7)
-        ///@notice used polynomial appox. constants because it would be much more expensive if trying to calculate integrals
-        int256 a1 = 254829592 * 1e9;
-        int256 a2 = -284496736 * 1e9;
-        int256 a3 = 142141374 * 1e9;
-        int256 a4 = -106122205 * 1e9;
-        int256 a5 = 102112745 * 1e9;
-        int256 p = 3275911 * 1e9;
-
-        int256 t = ONE / (ONE + (p * x) / ONE);
-        int256 y = (((((a5 * t) / ONE + a4) * t) / ONE + a3) * t) / ONE + a2;
-        y = ((y * t) / ONE + a1) * t;
-        y = (ONE - y) * expNegHalf(x);
-        return sign * y;
-    }
-
+    ///@notice computes the exponential function using the 2/2 Pade approximation
     function exp(int256 x) internal pure returns (int256) {
         int256 x2 = (x * x) / ONE;
         int256 numerator = ONE + (x / TWO) + (x2 / TWELFTH);
         int256 denominator = ONE - (x / TWO) + (x2 / TWELFTH);
         return (numerator * ONE) / denominator;
-    }
-   
-   function addSafe(int256 a, int256 b) internal pure returns (int256) {
-        int256 c = a + b;
-       // require(c >= a, "Addition overflow");
-        return c;
     }
 
     function mulDiv(int256 a, int256 b) internal pure returns (int256) {
